a718f575840741fdc81654dcd52f168dcd842655
Anvi Alex <epononanvialex@gmail.com>
2021-07-13 20:22:46 +0300

    reset config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './pic.jpeg';
-import Data from './data.xml';
-import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, imported
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add image to existing div
-  const myIcon =new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
   
   return element;
 }
