1a4a016b4fa4e77d1e42de673020abccb9af8faa
Anvi Alex <epononanvialex@gmail.com>
2021-07-13 19:54:11 +0300

    Load image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './pic.jpeg';
 
 function component() {
   const element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
+  // Add image to existing div
+  const myIcon =new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  
   return element;
 }
 
