86404916db0a45995c8686fe230af2dc2559d3e4
Anvi Alex <epononanvialex@gmail.com>
2021-07-13 21:02:42 +0300

    Output management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
+import printMe from './print.js'; 
 
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, imported
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-  return element;
-}
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-document.body.appendChild(component());
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
